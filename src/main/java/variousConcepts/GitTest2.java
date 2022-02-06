b5c38dfed30e1f666e3c379e8a4194c8c5475aea
shihabamin <amin.khandaker@gmail.com>
2022-02-06 12:42:03 -0600

    changed code

--- src/main/java/variousConcepts/GitTest2.java
+++ src/main/java/variousConcepts/GitTest2.java
@@ -1,5 +1,15 @@
 package variousConcepts;
 
 public class GitTest2 {
-
+	public static void main(String[] args) {
+		
+		System.out.println("git test");
+		printMe();
+	}
+	
+	public static void printMe() {
+		
+		System.out.println("get test 2");
+		System.out.println("get test 3");
+	}
 }
